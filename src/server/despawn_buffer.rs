9856ed9d1107ea697fa811b3ea57289d02709730
Hennadii Chernyshchyk <genaloner@gmail.com>
2023-12-27 23:04:36 +0200

    Buffer all despawns and removal to avoid missing events (#143)
    Co-authored-by: UkoeHB <37489173+UkoeHB@users.noreply.github.com>

---
+++ src/server/despawn_buffer.rs
@@ -0,0 +1,60 @@
+use bevy::prelude::*;
+use bevy_renet::renet::RenetServer;
+
+use super::{ServerPlugin, ServerSet};
+use crate::replicon_core::replication_rules::Replication;
+
+/// Treats removals of [`Replication`] component as despawns and stores them into [`DespawnBuffer`] resource.
+///
+/// Used to avoid missing events in case the server's tick policy is not [`TickPolicy::EveryFrame`].
+pub(super) struct DespawnBufferPlugin;
+
+impl Plugin for DespawnBufferPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<DespawnBuffer>().add_systems(
+            PostUpdate,
+            Self::detection_system
+                .before(ServerPlugin::replication_sending_system)
+                .in_set(ServerSet::Send)
+                .run_if(resource_exists::<RenetServer>()),
+        );
+    }
+}
+
+impl DespawnBufferPlugin {
+    pub(super) fn detection_system(
+        mut removed_replications: RemovedComponents<Replication>,
+        mut despawn_buffer: ResMut<DespawnBuffer>,
+    ) {
+        for entity in removed_replications.read() {
+            despawn_buffer.push(entity);
+        }
+    }
+}
+
+/// Buffer with all despawned entities.
+///
+/// Should be cleaned up manually.
+#[derive(Default, Resource, Deref, DerefMut)]
+pub(crate) struct DespawnBuffer(Vec<Entity>);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn despawns() {
+        let mut app = App::new();
+        app.add_plugins(DespawnBufferPlugin)
+            .insert_resource(RenetServer::new(Default::default()));
+
+        app.update();
+
+        app.world.spawn(Replication).despawn();
+
+        app.update();
+
+        let despawn_buffer = app.world.resource::<DespawnBuffer>();
+        assert_eq!(despawn_buffer.len(), 1);
+    }
+}
---
+++ src/server/removal_buffer.rs
@@ -0,0 +1,154 @@
+use bevy::{
+    ecs::{
+        component::ComponentId,
+        event::ManualEventReader,
+        removal_detection::{RemovedComponentEntity, RemovedComponentEvents},
+    },
+    prelude::*,
+    utils::{EntityHashMap, HashMap},
+};
+use bevy_renet::renet::RenetServer;
+
+use super::{
+    despawn_buffer::{DespawnBuffer, DespawnBufferPlugin},
+    ServerPlugin, ServerSet,
+};
+use crate::replicon_core::replication_rules::{ReplicationId, ReplicationRules};
+
+/// Buffers all replicated component removals in [`RemovalBuffer`] resource.
+///
+/// Used to avoid missing events.
+pub(super) struct RemovalBufferPlugin;
+
+impl Plugin for RemovalBufferPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<RemovalBuffer>().add_systems(
+            PostUpdate,
+            Self::detection_system
+                .after(DespawnBufferPlugin::detection_system)
+                .before(ServerPlugin::replication_sending_system)
+                .in_set(ServerSet::Send)
+                .run_if(resource_exists::<RenetServer>()),
+        );
+    }
+}
+
+impl RemovalBufferPlugin {
+    fn detection_system(
+        mut readers: Local<HashMap<ComponentId, ManualEventReader<RemovedComponentEntity>>>,
+        remove_events: &RemovedComponentEvents,
+        mut removal_buffer: ResMut<RemovalBuffer>,
+        replication_rules: Res<ReplicationRules>,
+        despawn_buffer: Res<DespawnBuffer>,
+    ) {
+        for (&component_id, &replication_id) in replication_rules.get_ids() {
+            for removals in remove_events.get(component_id).into_iter() {
+                let reader = readers.entry(component_id).or_default();
+                for entity in reader
+                    .read(removals)
+                    .cloned()
+                    .map(Into::into)
+                    .filter(|entity| !despawn_buffer.contains(entity))
+                {
+                    removal_buffer.insert(entity, replication_id);
+                }
+            }
+        }
+    }
+}
+
+/// Buffer with removed components.
+#[derive(Default, Resource)]
+pub(crate) struct RemovalBuffer {
+    /// Component removals grouped by entity.
+    removals: EntityHashMap<Entity, Vec<ReplicationId>>,
+
+    /// [`Vec`]'s from entity removals.
+    ///
+    /// All data is cleared before the insertion.
+    /// Stored to reuse allocated capacity.
+    component_buffer: Vec<Vec<ReplicationId>>,
+}
+
+impl RemovalBuffer {
+    /// Returns an iterator over entities and their removed components.
+    pub(super) fn iter(&self) -> impl Iterator<Item = (Entity, &[ReplicationId])> {
+        self.removals
+            .iter()
+            .map(|(&entity, components)| (entity, &**components))
+    }
+
+    /// Registers component removal for the specified entity.
+    fn insert(&mut self, entity: Entity, replication_id: ReplicationId) {
+        self.removals
+            .entry(entity)
+            .or_insert_with(|| self.component_buffer.pop().unwrap_or_default())
+            .push(replication_id);
+    }
+
+    /// Clears all removals.
+    ///
+    /// Keeps the allocated memory for reuse.
+    pub(super) fn clear(&mut self) {
+        self.component_buffer
+            .extend(self.removals.drain().map(|(_, mut components)| {
+                components.clear();
+                components
+            }));
+    }
+}
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+
+    use super::*;
+    use crate::replicon_core::replication_rules::{AppReplicationExt, Replication};
+
+    #[test]
+    fn removals() {
+        let mut app = App::new();
+        app.add_plugins((DespawnBufferPlugin, RemovalBufferPlugin))
+            .insert_resource(RenetServer::new(Default::default()))
+            .init_resource::<ReplicationRules>()
+            .replicate::<DummyComponent>();
+
+        app.update();
+
+        app.world
+            .spawn((DummyComponent, Replication))
+            .remove::<DummyComponent>();
+
+        app.update();
+
+        let mut removal_buffer = app.world.resource_mut::<RemovalBuffer>();
+        assert_eq!(removal_buffer.removals.len(), 1);
+
+        removal_buffer.clear();
+        assert!(removal_buffer.removals.is_empty());
+        assert_eq!(removal_buffer.component_buffer.len(), 1);
+    }
+
+    #[test]
+    fn despawn_ignore() {
+        let mut app = App::new();
+        app.add_plugins((DespawnBufferPlugin, RemovalBufferPlugin))
+            .insert_resource(RenetServer::new(Default::default()))
+            .init_resource::<ReplicationRules>()
+            .replicate::<DummyComponent>();
+
+        app.update();
+
+        app.world.spawn((DummyComponent, Replication)).despawn();
+
+        app.update();
+
+        let removal_buffer = app.world.resource::<RemovalBuffer>();
+        assert!(
+            removal_buffer.removals.is_empty(),
+            "despawns shouldn't be counted as removals"
+        );
+    }
+
+    #[derive(Serialize, Deserialize, Component)]
+    struct DummyComponent;
+}
