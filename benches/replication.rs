534f72c796334b4b803c6e30b6753d9256798b29
UkoeHB <37489173+UkoeHB@users.noreply.github.com>
2024-01-09 05:41:12 -0600

    Add string and struct benchmarks (#157)
    Co-authored-by: Hennadii Chernyshchyk <genaloner@gmail.com>

--- benches/replication.rs
+++ benches/replication.rs
@@ -1,39 +1,77 @@
 #[path = "../tests/connect/mod.rs"]
 mod connect;
 
-use std::time::{Duration, Instant};
+use std::{
+    any,
+    time::{Duration, Instant},
+};
 
 use bevy::prelude::*;
 use bevy_replicon::prelude::*;
 use criterion::{criterion_group, criterion_main, Criterion};
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use spin_sleep::{SpinSleeper, SpinStrategy};
 
-#[derive(Component, Clone, Copy, Serialize, Deserialize)]
-struct DummyComponent(usize);
+#[derive(Clone, Component, Default, Deserialize, Serialize)]
+struct UsizeComponent(usize);
 
-fn replication(c: &mut Criterion) {
+#[derive(Component, Clone, Serialize, Deserialize)]
+struct StringComponent(String);
+
+impl Default for StringComponent {
+    fn default() -> Self {
+        // Max size string before we hit Renet's message rate limit of 60Kb
+        Self(".".repeat(54))
+    }
+}
+
+#[derive(Component, Clone, Serialize, Deserialize)]
+struct StructComponent {
+    x: u32,
+    y: u32,
+    b: f32,
+    a: f32,
+    n: String,
+}
+
+impl Default for StructComponent {
+    fn default() -> Self {
+        Self {
+            x: 22u32,
+            y: 22u32,
+            b: 1.5f32,
+            a: 20.0f32,
+            n: String::from("abcdef123"),
+        }
+    }
+}
+
+fn replication<C: Component + Default + Serialize + DeserializeOwned + Clone>(c: &mut Criterion) {
     const ENTITIES: u32 = 1000;
     const SOCKET_WAIT: Duration = Duration::from_millis(5); // Sometimes it takes time for socket to receive all data.
+    const MODULE_PREFIX_LEN: usize = module_path!().len() + 2;
+
+    let mut name = any::type_name::<C>();
+    name = &name[MODULE_PREFIX_LEN..];
 
     // Use spinner to keep CPU hot in the schedule for stable benchmark results.
     let sleeper = SpinSleeper::new(1_000_000_000).with_spin_strategy(SpinStrategy::SpinLoopHint);
 
     for clients in [1, 20] {
-        c.bench_function(&format!("init send, {clients} client(s)"), |b| {
+        c.bench_function(&format!("{name}, init send, {clients} client(s)"), |b| {
             b.iter_custom(|iter| {
                 let mut elapsed = Duration::ZERO;
                 for _ in 0..iter {
-                    let mut server_app = create_app();
+                    let mut server_app = create_app::<C>();
                     let mut client_apps = Vec::new();
                     for _ in 0..clients {
-                        client_apps.push(create_app());
+                        client_apps.push(create_app::<C>());
                     }
                     connect::multiple_clients(&mut server_app, &mut client_apps);
 
                     server_app
                         .world
-                        .spawn_batch([(Replication, DummyComponent(0)); ENTITIES as usize]);
+                        .spawn_batch(vec![(Replication, C::default()); ENTITIES as usize]);
 
                     let instant = Instant::now();
                     server_app.update();
@@ -50,19 +88,19 @@ fn replication(c: &mut Criterion) {
             })
         });
 
-        c.bench_function(&format!("update send, {clients} client(s)"), |b| {
+        c.bench_function(&format!("{name}, update send, {clients} client(s)"), |b| {
             b.iter_custom(|iter| {
-                let mut server_app = create_app();
+                let mut server_app = create_app::<C>();
                 let mut client_apps = Vec::new();
                 for _ in 0..clients {
-                    client_apps.push(create_app());
+                    client_apps.push(create_app::<C>());
                 }
                 connect::multiple_clients(&mut server_app, &mut client_apps);
 
                 server_app
                     .world
-                    .spawn_batch([(Replication, DummyComponent(0)); ENTITIES as usize]);
-                let mut query = server_app.world.query::<&mut DummyComponent>();
+                    .spawn_batch(vec![(Replication, C::default()); ENTITIES as usize]);
+                let mut query = server_app.world.query::<&mut C>();
 
                 server_app.update();
                 sleeper.sleep(SOCKET_WAIT);
@@ -73,8 +111,8 @@ fn replication(c: &mut Criterion) {
 
                 let mut elapsed = Duration::ZERO;
                 for _ in 0..iter {
-                    for mut dummy_component in query.iter_mut(&mut server_app.world) {
-                        dummy_component.0 += 1;
+                    for mut component in query.iter_mut(&mut server_app.world) {
+                        component.set_changed();
                     }
 
                     sleeper.sleep(SOCKET_WAIT);
@@ -94,17 +132,17 @@ fn replication(c: &mut Criterion) {
         });
     }
 
-    c.bench_function("init receive", |b| {
+    c.bench_function(&format!("{name}, init receive"), |b| {
         b.iter_custom(|iter| {
             let mut elapsed = Duration::ZERO;
             for _ in 0..iter {
-                let mut server_app = create_app();
-                let mut client_app = create_app();
+                let mut server_app = create_app::<C>();
+                let mut client_app = create_app::<C>();
                 connect::single_client(&mut server_app, &mut client_app);
 
                 server_app
                     .world
-                    .spawn_batch([(Replication, DummyComponent(0)); ENTITIES as usize]);
+                    .spawn_batch(vec![(Replication, C::default()); ENTITIES as usize]);
 
                 server_app.update();
                 sleeper.sleep(SOCKET_WAIT);
@@ -119,16 +157,16 @@ fn replication(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("update receive", |b| {
+    c.bench_function(&format!("{name}, update receive"), |b| {
         b.iter_custom(|iter| {
-            let mut server_app = create_app();
-            let mut client_app = create_app();
+            let mut server_app = create_app::<C>();
+            let mut client_app = create_app::<C>();
             connect::single_client(&mut server_app, &mut client_app);
 
             server_app
                 .world
-                .spawn_batch([(Replication, DummyComponent(0)); ENTITIES as usize]);
-            let mut query = server_app.world.query::<&mut DummyComponent>();
+                .spawn_batch(vec![(Replication, C::default()); ENTITIES as usize]);
+            let mut query = server_app.world.query::<&mut C>();
 
             server_app.update();
             sleeper.sleep(SOCKET_WAIT);
@@ -137,8 +175,8 @@ fn replication(c: &mut Criterion) {
 
             let mut elapsed = Duration::ZERO;
             for _ in 0..iter {
-                for mut dummy_component in query.iter_mut(&mut server_app.world) {
-                    dummy_component.0 += 1;
+                for mut component in query.iter_mut(&mut server_app.world) {
+                    component.set_changed();
                 }
 
                 sleeper.sleep(SOCKET_WAIT);
@@ -157,7 +195,7 @@ fn replication(c: &mut Criterion) {
     });
 }
 
-fn create_app() -> App {
+fn create_app<C: Component + Serialize + DeserializeOwned>() -> App {
     let mut app = App::new();
     app.add_plugins((
         MinimalPlugins,
@@ -166,14 +204,24 @@ fn create_app() -> App {
             ..Default::default()
         }),
     ))
-    .replicate::<DummyComponent>();
+    .replicate::<C>();
 
     app
 }
 
 criterion_group! {
-    name = benches;
+    name = int_benches;
     config = Criterion::default().sample_size(20);
-    targets = replication
+    targets = replication::<UsizeComponent>
 }
-criterion_main!(benches);
+criterion_group! {
+    name = string_benches;
+    config = Criterion::default().sample_size(20);
+    targets = replication::<StringComponent>
+}
+criterion_group! {
+    name = struct_benches;
+    config = Criterion::default().sample_size(20);
+    targets = replication::<StructComponent>
+}
+criterion_main!(int_benches, string_benches, struct_benches);
