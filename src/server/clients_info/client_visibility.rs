53cb84c448b24ca5f0a0110328af7f3a734a4e50
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-01-14 16:22:05 +0200

    Turn `ClientVisibility` into a struct with the inner enum
    Necessary to make fields private.

--- src/server/clients_info/client_visibility.rs
+++ src/server/clients_info/client_visibility.rs
@@ -6,7 +6,205 @@ use bevy::{
 use super::VisibilityPolicy;
 
 /// Entity visibility settings for a client.
-pub enum ClientVisibility {
+pub struct ClientVisibility(VisibilityFilter);
+
+impl ClientVisibility {
+    /// Creates a new instance based on the preconfigured policy.
+    pub(super) fn new(policy: VisibilityPolicy) -> Self {
+        match policy {
+            VisibilityPolicy::All => Self(VisibilityFilter::All {
+                just_connected: true,
+            }),
+            VisibilityPolicy::Blacklist => Self(VisibilityFilter::Blacklist {
+                list: Default::default(),
+                added: Default::default(),
+                removed: Default::default(),
+            }),
+            VisibilityPolicy::Whitelist => Self(VisibilityFilter::Whitelist {
+                list: Default::default(),
+                added: Default::default(),
+                removed: Default::default(),
+            }),
+        }
+    }
+
+    /// Resets the state to as it was after [`Self::new`].
+    pub(super) fn clear(&mut self) {
+        match &mut self.0 {
+            VisibilityFilter::All { just_connected } => *just_connected = true,
+            VisibilityFilter::Blacklist {
+                list,
+                added,
+                removed,
+            }
+            | VisibilityFilter::Whitelist {
+                list,
+                added,
+                removed,
+            } => {
+                list.clear();
+                added.clear();
+                removed.clear();
+            }
+        }
+    }
+
+    /// Marks all entities as not "just added" and clears the removed.
+    ///
+    /// Should be called after each tick.
+    pub(crate) fn update(&mut self) {
+        match &mut self.0 {
+            VisibilityFilter::All { just_connected } => *just_connected = false,
+            VisibilityFilter::Blacklist {
+                list,
+                added,
+                removed,
+            } => {
+                for entity in removed.drain() {
+                    list.remove(&entity);
+                }
+                added.clear();
+            }
+            VisibilityFilter::Whitelist {
+                list,
+                added,
+                removed,
+            } => {
+                for entity in added.drain() {
+                    list.insert(entity, false);
+                }
+                removed.clear();
+            }
+        }
+    }
+
+    /// Removes a despawned entity tracked by this client.
+    pub(super) fn remove_despawned(&mut self, entity: Entity) {
+        match &mut self.0 {
+            VisibilityFilter::All { .. } => (),
+            VisibilityFilter::Blacklist {
+                list,
+                added,
+                removed,
+            } => {
+                if let Some(just_removed) = list.get_mut(&entity) {
+                    *just_removed = true;
+                    removed.remove(&entity);
+                    added.remove(&entity);
+                }
+            }
+            VisibilityFilter::Whitelist {
+                list,
+                added,
+                removed,
+            } => {
+                if list.remove(&entity).is_some() {
+                    added.remove(&entity);
+                    removed.remove(&entity);
+                }
+            }
+        }
+    }
+
+    /// Returns an iterator of entities that lost visibility at this tick.
+    pub(super) fn iter_lost_visibility(&self) -> impl Iterator<Item = Entity> + '_ {
+        match &self.0 {
+            VisibilityFilter::All { .. } => VisibilityLostIter::AllVisible,
+            VisibilityFilter::Blacklist { added, .. } => {
+                VisibilityLostIter::Lost(added.iter().copied())
+            }
+            VisibilityFilter::Whitelist { removed, .. } => {
+                VisibilityLostIter::Lost(removed.iter().copied())
+            }
+        }
+    }
+
+    /// Sets visibility for specific entity.
+    ///
+    /// Does nothing if visibility policy for the server plugin is set to [`VisibilityPolicy::All`].
+    pub fn set_visible(&mut self, entity: Entity, visibile: bool) {
+        match &mut self.0 {
+            VisibilityFilter::All { .. } => {
+                if visibile {
+                    debug!(
+                        "ignoring visibility enable due to {:?}",
+                        VisibilityPolicy::All
+                    );
+                } else {
+                    warn!(
+                        "ignoring visibility disable due to {:?}",
+                        VisibilityPolicy::All
+                    );
+                }
+            }
+            VisibilityFilter::Blacklist {
+                list,
+                added,
+                removed,
+            } => {
+                if !visibile {
+                    if list.insert(entity, false).is_none() {
+                        removed.remove(&entity);
+                        added.insert(entity);
+                    }
+                } else if list.remove(&entity).is_some() {
+                    removed.insert(entity);
+                    added.remove(&entity);
+                }
+            }
+            VisibilityFilter::Whitelist {
+                list,
+                added,
+                removed,
+            } => {
+                if visibile {
+                    if list.insert(entity, true).is_none() {
+                        removed.remove(&entity);
+                        added.insert(entity);
+                    }
+                } else if list.remove(&entity).is_some() {
+                    removed.insert(entity);
+                    added.remove(&entity);
+                }
+            }
+        }
+    }
+
+    /// Gets visibility for specific entity.
+    pub fn is_visible(&self, entity: Entity) -> bool {
+        match &self.0 {
+            VisibilityFilter::All { .. } => true,
+            VisibilityFilter::Blacklist { list, .. } => !list.contains_key(&entity),
+            VisibilityFilter::Whitelist { list, .. } => list.contains_key(&entity),
+        }
+    }
+
+    /// Gets visibility with change information included for specific entity.
+    pub(crate) fn get_info(&self, entity: Entity) -> VisibilityInfo {
+        match &self.0 {
+            VisibilityFilter::All { just_connected } => {
+                if *just_connected {
+                    VisibilityInfo::Gained
+                } else {
+                    VisibilityInfo::Maintained
+                }
+            }
+            VisibilityFilter::Blacklist { list, .. } => match list.get(&entity) {
+                Some(true) => VisibilityInfo::Gained,
+                Some(false) => VisibilityInfo::None,
+                None => VisibilityInfo::Maintained,
+            },
+            VisibilityFilter::Whitelist { list, .. } => match list.get(&entity) {
+                Some(true) => VisibilityInfo::Gained,
+                Some(false) => VisibilityInfo::Maintained,
+                None => VisibilityInfo::None,
+            },
+        }
+    }
+}
+
+/// Since enums can't have private fields, it's wrapped into [`ClientVisibility`] to make fields private, while keeping the struct public.
+enum VisibilityFilter {
     All {
         /// Indicates that the client has just connected to the server.
         just_connected: bool,
@@ -27,201 +225,6 @@ pub enum ClientVisibility {
         /// All entities that were removed from the list on this tick.
         removed: EntityHashSet<Entity>,
     },
-}
-
-impl ClientVisibility {
-    /// Creates a new instance based on the preconfigured policy.
-    pub(super) fn new(policy: VisibilityPolicy) -> Self {
-        match policy {
-            VisibilityPolicy::All => Self::All {
-                just_connected: true,
-            },
-            VisibilityPolicy::Blacklist => Self::Blacklist {
-                list: Default::default(),
-                added: Default::default(),
-                removed: Default::default(),
-            },
-            VisibilityPolicy::Whitelist => Self::Whitelist {
-                list: Default::default(),
-                added: Default::default(),
-                removed: Default::default(),
-            },
-        }
-    }
-
-    /// Resets the state to as it was after [`Self::new`].
-    pub(super) fn clear(&mut self) {
-        match self {
-            ClientVisibility::All { just_connected } => *just_connected = true,
-            ClientVisibility::Blacklist {
-                list,
-                added,
-                removed,
-            }
-            | ClientVisibility::Whitelist {
-                list,
-                added,
-                removed,
-            } => {
-                list.clear();
-                added.clear();
-                removed.clear();
-            }
-        }
-    }
-
-    /// Marks all entities as not "just added" and clears the removed.
-    ///
-    /// Should be called after each tick.
-    pub(crate) fn update(&mut self) {
-        match self {
-            ClientVisibility::All { just_connected } => *just_connected = false,
-            ClientVisibility::Blacklist {
-                list,
-                added,
-                removed,
-            } => {
-                for entity in removed.drain() {
-                    list.remove(&entity);
-                }
-                added.clear();
-            }
-            ClientVisibility::Whitelist {
-                list,
-                added,
-                removed,
-            } => {
-                for entity in added.drain() {
-                    list.insert(entity, false);
-                }
-                removed.clear();
-            }
-        }
-    }
-
-    /// Removes a despawned entity tracked by this client.
-    pub(super) fn remove_despawned(&mut self, entity: Entity) {
-        match self {
-            ClientVisibility::All { .. } => (),
-            ClientVisibility::Blacklist {
-                list,
-                added,
-                removed,
-            } => {
-                if let Some(just_removed) = list.get_mut(&entity) {
-                    *just_removed = true;
-                    removed.remove(&entity);
-                    added.remove(&entity);
-                }
-            }
-            ClientVisibility::Whitelist {
-                list,
-                added,
-                removed,
-            } => {
-                if list.remove(&entity).is_some() {
-                    added.remove(&entity);
-                    removed.remove(&entity);
-                }
-            }
-        }
-    }
-
-    /// Returns an iterator of entities that lost visibility at this tick.
-    pub(super) fn iter_lost_visibility(&self) -> impl Iterator<Item = Entity> + '_ {
-        match self {
-            ClientVisibility::All { .. } => VisibilityLostIter::AllVisible,
-            ClientVisibility::Blacklist { added, .. } => {
-                VisibilityLostIter::Lost(added.iter().copied())
-            }
-            ClientVisibility::Whitelist { removed, .. } => {
-                VisibilityLostIter::Lost(removed.iter().copied())
-            }
-        }
-    }
-
-    /// Sets visibility for specific entity.
-    ///
-    /// Does nothing if visibility policy for the server plugin is set to [`VisibilityPolicy::All`].
-    pub fn set_visible(&mut self, entity: Entity, visibile: bool) {
-        match self {
-            ClientVisibility::All { .. } => {
-                if visibile {
-                    debug!(
-                        "ignoring visibility enable due to {:?}",
-                        VisibilityPolicy::All
-                    );
-                } else {
-                    warn!(
-                        "ignoring visibility disable due to {:?}",
-                        VisibilityPolicy::All
-                    );
-                }
-            }
-            ClientVisibility::Blacklist {
-                list,
-                added,
-                removed,
-            } => {
-                if !visibile {
-                    if list.insert(entity, false).is_none() {
-                        removed.remove(&entity);
-                        added.insert(entity);
-                    }
-                } else if list.remove(&entity).is_some() {
-                    removed.insert(entity);
-                    added.remove(&entity);
-                }
-            }
-            ClientVisibility::Whitelist {
-                list,
-                added,
-                removed,
-            } => {
-                if visibile {
-                    if list.insert(entity, true).is_none() {
-                        removed.remove(&entity);
-                        added.insert(entity);
-                    }
-                } else if list.remove(&entity).is_some() {
-                    removed.insert(entity);
-                    added.remove(&entity);
-                }
-            }
-        }
-    }
-
-    /// Gets visibility for specific entity.
-    pub fn is_visible(&self, entity: Entity) -> bool {
-        match self {
-            ClientVisibility::All { .. } => true,
-            ClientVisibility::Blacklist { list, .. } => !list.contains_key(&entity),
-            ClientVisibility::Whitelist { list, .. } => list.contains_key(&entity),
-        }
-    }
-
-    /// Gets visibility with change information included for specific entity.
-    pub(crate) fn get_info(&self, entity: Entity) -> VisibilityInfo {
-        match self {
-            ClientVisibility::All { just_connected } => {
-                if *just_connected {
-                    VisibilityInfo::Gained
-                } else {
-                    VisibilityInfo::Maintained
-                }
-            }
-            ClientVisibility::Blacklist { list, .. } => match list.get(&entity) {
-                Some(true) => VisibilityInfo::Gained,
-                Some(false) => VisibilityInfo::None,
-                None => VisibilityInfo::Maintained,
-            },
-            ClientVisibility::Whitelist { list, .. } => match list.get(&entity) {
-                Some(true) => VisibilityInfo::Gained,
-                Some(false) => VisibilityInfo::Maintained,
-                None => VisibilityInfo::None,
-            },
-        }
-    }
 }
 
 #[derive(PartialEq)]
