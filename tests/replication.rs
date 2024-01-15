c0b2d8af2167141d7f6b35e3053a970c8030263a
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-01-15 23:54:43 +0200

    Add whitelist despawn test

--- tests/replication.rs
+++ tests/replication.rs
@@ -988,6 +988,43 @@ fn whitelist_visibility() {
 }
 
 #[test]
+fn whitelist_despawn_visibility() {
+    let mut server_app = App::new();
+    let mut client_app = App::new();
+    for app in [&mut server_app, &mut client_app] {
+        app.add_plugins((
+            MinimalPlugins,
+            ReplicationPlugins.set(ServerPlugin {
+                tick_policy: TickPolicy::EveryFrame,
+                visibility_policy: VisibilityPolicy::Whitelist,
+                ..Default::default()
+            }),
+        ))
+        .replicate::<TableComponent>();
+    }
+
+    connect::single_client(&mut server_app, &mut client_app);
+
+    let server_entity = server_app.world.spawn((Replication, TableComponent)).id();
+
+    let client_transport = client_app.world.resource::<NetcodeClientTransport>();
+    let client_id = ClientId::from_raw(client_transport.client_id());
+    let mut clients_info = server_app.world.resource_mut::<ClientsInfo>();
+    let visibility = clients_info.client_mut(client_id).visibility_mut();
+    visibility.set_visibility(server_entity, true);
+    server_app.world.despawn(server_entity);
+
+    server_app.update();
+    client_app.update();
+
+    assert!(client_app.world.entities().is_empty());
+
+    let mut clients_info = server_app.world.resource_mut::<ClientsInfo>();
+    let visibility = clients_info.client_mut(client_id).visibility_mut();
+    assert!(!visibility.is_visible(server_entity));
+}
+
+#[test]
 fn replication_into_scene() {
     let mut app = App::new();
     app.add_plugins(ReplicationPlugins)
