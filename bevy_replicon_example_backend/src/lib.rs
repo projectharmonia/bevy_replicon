5d3cabb38161db8d140c9d01ce39b9e0336456c5
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-01-25 21:05:00 +0200

    Add client and server features
    I don't think examples in my repo utilize it,
    but I want this crate to be an example of how to write backend crates.
    
    Also some third-party crates may utilize it for their examples.

--- bevy_replicon_example_backend/src/lib.rs
+++ bevy_replicon_example_backend/src/lib.rs
@@ -2,8 +2,12 @@
 //! This transport does not implement any reliability or security features.
 //! DO NOT USE in a real project
 
+#[cfg(feature = "client")]
 mod client;
-pub use client::*;
-
+#[cfg(feature = "server")]
 mod server;
+
+#[cfg(feature = "client")]
+pub use client::*;
+#[cfg(feature = "server")]
 pub use server::*;
