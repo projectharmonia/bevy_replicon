1c22888da9af90001c942c2e3c651e6ea9de1833
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-01-25 12:52:51 +0200

    Uncomment last ignore

--- tests/server_trigger.rs
+++ tests/server_trigger.rs
@@ -119,7 +119,6 @@ fn sending_receiving_and_mapping() {
 }
 
 #[test]
-#[ignore = "fail on CI, but passes locally, needs investigation"]
 fn sending_receiving_without_plugins() {
     let mut server_app = App::new();
     let mut client_app = App::new();
