9966831a34de7cf93d53d50198d2694167962a3b
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-01-25 20:49:38 +0200

    Put all systems under plugin impls
    For consistency with the rest of the repo.

--- bevy_replicon_example_backend/src/client.rs
+++ bevy_replicon_example_backend/src/client.rs
@@ -15,19 +15,65 @@ impl Plugin for RepliconExampleClientPlugin {
         app.add_systems(
             PreUpdate,
             (
-                set_disconnected.run_if(resource_removed::<ExampleClientSocket>),
-                set_connected.run_if(resource_added::<ExampleClientSocket>),
-                receive_packets.never_param_warn(),
+                Self::set_disconnected.run_if(resource_removed::<ExampleClientSocket>),
+                Self::set_connected.run_if(resource_added::<ExampleClientSocket>),
+                Self::receive_packets.never_param_warn(),
             )
                 .chain()
                 .in_set(ClientSet::ReceivePackets),
         )
         .add_systems(
             PostUpdate,
-            send_packets
+            Self::send_packets
                 .never_param_warn()
                 .in_set(ClientSet::SendPackets),
         );
+    }
+}
+
+impl RepliconExampleClientPlugin {
+    fn set_disconnected(mut client: ResMut<RepliconClient>) {
+        client.set_status(RepliconClientStatus::Disconnected);
+    }
+
+    fn set_connected(mut client: ResMut<RepliconClient>) {
+        client.set_status(RepliconClientStatus::Connected { client_id: None });
+    }
+
+    fn receive_packets(
+        mut commands: Commands,
+        socket: Res<ExampleClientSocket>,
+        mut replicon_client: ResMut<RepliconClient>,
+    ) {
+        let mut buf = [0u8; 1502];
+        loop {
+            let size = match socket.recv(&mut buf) {
+                Ok(s) => s,
+                Err(e) => {
+                    if e.kind() != std::io::ErrorKind::WouldBlock {
+                        commands.remove_resource::<ExampleClientSocket>();
+                        error!("disconnecting due to network error: {e}");
+                    }
+                    return;
+                }
+            };
+            if size < 1 {
+                commands.remove_resource::<ExampleClientSocket>();
+                debug!("got empty packet, disconnecting");
+                return;
+            }
+            let channel_id = buf[0];
+            replicon_client.insert_received(channel_id, Vec::from(&buf[1..size]));
+        }
+    }
+
+    fn send_packets(socket: Res<ExampleClientSocket>, mut replicon_client: ResMut<RepliconClient>) {
+        for (channel_id, message) in replicon_client.drain_sent() {
+            let mut data = Vec::with_capacity(message.len() + mem::size_of_val(&channel_id));
+            data.push(channel_id);
+            data.extend(message);
+            socket.send(&data).unwrap();
+        }
     }
 }
 
@@ -42,49 +88,5 @@ impl ExampleClientSocket {
         socket.set_nonblocking(true)?;
         socket.connect((Ipv4Addr::LOCALHOST, port))?;
         Ok(Self(socket))
-    }
-}
-
-fn set_disconnected(mut client: ResMut<RepliconClient>) {
-    client.set_status(RepliconClientStatus::Disconnected);
-}
-
-fn set_connected(mut client: ResMut<RepliconClient>) {
-    client.set_status(RepliconClientStatus::Connected { client_id: None });
-}
-
-fn receive_packets(
-    mut commands: Commands,
-    socket: Res<ExampleClientSocket>,
-    mut replicon_client: ResMut<RepliconClient>,
-) {
-    let mut buf = [0u8; 1502];
-    loop {
-        let size = match socket.recv(&mut buf) {
-            Ok(s) => s,
-            Err(e) => {
-                if e.kind() != std::io::ErrorKind::WouldBlock {
-                    commands.remove_resource::<ExampleClientSocket>();
-                    error!("disconnecting due to network error: {e}");
-                }
-                return;
-            }
-        };
-        if size < 1 {
-            commands.remove_resource::<ExampleClientSocket>();
-            debug!("got empty packet, disconnecting");
-            return;
-        }
-        let channel_id = buf[0];
-        replicon_client.insert_received(channel_id, Vec::from(&buf[1..size]));
-    }
-}
-
-fn send_packets(socket: Res<ExampleClientSocket>, mut replicon_client: ResMut<RepliconClient>) {
-    for (channel_id, message) in replicon_client.drain_sent() {
-        let mut data = Vec::with_capacity(message.len() + mem::size_of_val(&channel_id));
-        data.push(channel_id);
-        data.extend(message);
-        socket.send(&data).unwrap();
     }
 }
--- bevy_replicon_example_backend/src/server.rs
+++ bevy_replicon_example_backend/src/server.rs
@@ -15,19 +15,73 @@ impl Plugin for RepliconExampleServerPlugin {
         app.add_systems(
             PreUpdate,
             (
-                set_stopped.run_if(resource_removed::<ExampleServerSocket>),
-                set_running.run_if(resource_added::<ExampleServerSocket>),
-                receive_packets.never_param_warn(),
+                Self::set_stopped.run_if(resource_removed::<ExampleServerSocket>),
+                Self::set_running.run_if(resource_added::<ExampleServerSocket>),
+                Self::receive_packets.never_param_warn(),
             )
                 .chain()
                 .in_set(ServerSet::ReceivePackets),
         )
         .add_systems(
             PostUpdate,
-            send_packets
+            Self::send_packets
                 .never_param_warn()
                 .in_set(ServerSet::SendPackets),
         );
+    }
+}
+
+impl RepliconExampleServerPlugin {
+    fn set_stopped(mut server: ResMut<RepliconServer>) {
+        server.set_running(false);
+    }
+
+    fn set_running(mut server: ResMut<RepliconServer>) {
+        server.set_running(true);
+    }
+
+    fn receive_packets(
+        socket: Res<ExampleServerSocket>,
+        mut replicon_server: ResMut<RepliconServer>,
+        clients: Res<ConnectedClients>,
+        mut server_events: EventWriter<ServerEvent>,
+    ) {
+        let mut buf = [0u8; 1502];
+        loop {
+            let (size, addr) = match socket.recv_from(&mut buf) {
+                Ok(v) => v,
+                Err(e) => {
+                    if e.kind() == io::ErrorKind::WouldBlock {
+                        return;
+                    }
+                    error!("failed to receive packet: {e}");
+                    continue;
+                }
+            };
+
+            let client_id = ClientId::new(addr.port() as u64);
+            if size < 1 {
+                error!("received empty packet from `{client_id:?}`");
+                continue;
+            }
+            let channel_id = buf[0];
+            if !clients.iter().any(|c| c.id() == client_id) {
+                server_events.send(ServerEvent::ClientConnected { client_id });
+            }
+            replicon_server.insert_received(client_id, channel_id, Vec::from(&buf[1..size]));
+        }
+    }
+
+    fn send_packets(socket: Res<ExampleServerSocket>, mut replicon_server: ResMut<RepliconServer>) {
+        for (client_id, channel_id, message) in replicon_server.drain_sent() {
+            let mut data = Vec::with_capacity(message.len() + mem::size_of_val(&channel_id));
+            data.push(channel_id);
+            data.extend(message);
+            let port = client_id.get() as u16;
+            socket
+                .send_to(&data, (socket.local_addr().unwrap().ip(), port))
+                .unwrap();
+        }
     }
 }
 
@@ -41,57 +95,5 @@ impl ExampleServerSocket {
         let socket = UdpSocket::bind((Ipv4Addr::LOCALHOST, port))?;
         socket.set_nonblocking(true)?;
         Ok(Self(socket))
-    }
-}
-
-fn set_stopped(mut server: ResMut<RepliconServer>) {
-    server.set_running(false);
-}
-
-fn set_running(mut server: ResMut<RepliconServer>) {
-    server.set_running(true);
-}
-
-fn receive_packets(
-    socket: Res<ExampleServerSocket>,
-    mut replicon_server: ResMut<RepliconServer>,
-    clients: Res<ConnectedClients>,
-    mut server_events: EventWriter<ServerEvent>,
-) {
-    let mut buf = [0u8; 1502];
-    loop {
-        let (size, addr) = match socket.recv_from(&mut buf) {
-            Ok(v) => v,
-            Err(e) => {
-                if e.kind() == io::ErrorKind::WouldBlock {
-                    return;
-                }
-                error!("failed to receive packet: {e}");
-                continue;
-            }
-        };
-
-        let client_id = ClientId::new(addr.port() as u64);
-        if size < 1 {
-            error!("received empty packet from `{client_id:?}`");
-            continue;
-        }
-        let channel_id = buf[0];
-        if !clients.iter().any(|c| c.id() == client_id) {
-            server_events.send(ServerEvent::ClientConnected { client_id });
-        }
-        replicon_server.insert_received(client_id, channel_id, Vec::from(&buf[1..size]));
-    }
-}
-
-fn send_packets(socket: Res<ExampleServerSocket>, mut replicon_server: ResMut<RepliconServer>) {
-    for (client_id, channel_id, message) in replicon_server.drain_sent() {
-        let mut data = Vec::with_capacity(message.len() + mem::size_of_val(&channel_id));
-        data.push(channel_id);
-        data.extend(message);
-        let port = client_id.get() as u16;
-        socket
-            .send_to(&data, (socket.local_addr().unwrap().ip(), port))
-            .unwrap();
     }
 }
